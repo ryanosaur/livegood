9c734bdd231fa93d1db630d63c4005ae4273654f
Ryano <ryan.justin.taylor@gmail.com>
2015-06-29 22:50:44 -0700

    copied livegood webpages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Live Good' });
+});
+
+router.get('/mission', function(req, res, next) {
+  res.render('mission', { title: 'Live Good | Our Mission' });
 });
 
 module.exports = router;
